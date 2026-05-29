99304ec8ebf8dcabc14b8e75bdc86da6343cedef
Sara Fira <sarafira26@gmail.com>
2026-05-29 16:24:56 +0300

    Update script.js

--- free-code-camp/JS-sentence-maker/script.js
+++ free-code-camp/JS-sentence-maker/script.js
@@ -1,1 +1,32 @@
-tu bydzie kod
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+adjective = "black";
+noun = "cat";
+verb = "sleeps";
+place = "room";
+adjective2 = "ginger";
+noun2 = "dashound";
+
+let firstStory;
+
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun +" lived in a " + place + " and had "+ adjective2 + " nostrils that blew fire when it was " +  verb + "."
+
+console.log("First story: " + firstStory);
+
+adjective = "white";
+noun = "dog";
+verb = "runs";
+place = "corridor";
+adjective2 = "silver";
+noun2 = "spoon";
+
+let secondStory;
+
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun +" lived in a " + place + " and had "+ adjective2 + " nostrils that blew fire when it was " +  verb + "."
+
+console.log("Second story: " + secondStory);
