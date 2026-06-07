d138a44fde50eae51ccbd40faaf81b3f5d9ace67
Sara Fira <sarafira26@gmail.com>
2026-06-07 13:33:06 +0300

    Update script.js

--- free-code-camp/card-counting-assistant/script.js
+++ free-code-camp/card-counting-assistant/script.js
@@ -1,1 +1,38 @@
+let count = 0;
 
+function cardCounter(card) {
+if (card === 2) {
+  count = count + 1;
+} else if (card === 3) {
+  count = count + 1
+ } else if (card === 4) {
+  count = count + 1 
+ } else if (card === 5) {
+  count = count + 1
+ } else if (card === 6) {
+  count = count + 1
+ } else if (card === 7) {
+  count = count;
+ } else if (card === 8) {
+  count = count;
+ } else if (card === 9) {
+  count = count;
+ } else if (card === 10) {
+  count = count - 1;
+ } else if (card === "J") {
+  count = count - 1;
+ } else if (card === "Q") {
+  count = count - 1;
+ } else if (card === "K") {
+  count = count - 1;
+ } else if (card === "A") {
+  count = count - 1;
+ }
+
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+
+}
