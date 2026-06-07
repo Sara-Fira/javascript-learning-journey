2e9d5b436b75053688e241bd93d4180e8594249d
Sara Fira <sarafira26@gmail.com>
2026-06-07 12:44:46 +0300

    Update script.js

--- free-code-camp/email-masker/script.js
+++ free-code-camp/email-masker/script.js
@@ -1,1 +1,15 @@
-a
+function maskEmail(email) {
+  const atIndex = email.indexOf("@");
+  const username = email.slice(0, atIndex);
+  const domain = email.slice(atIndex);
+
+  const firstChar = username[0];
+  const lastChar = username[username.length - 1];
+  const maskedPart = "*".repeat(username.length - 2);
+
+  return firstChar + maskedPart + lastChar + domain;
+}
+
+const email = "apple.pie@example.com";
+
+console.log(maskEmail(email));
