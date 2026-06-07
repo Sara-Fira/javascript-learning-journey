5bcb6dcd520b9f91c13962d0589e8cd289468279
Sara Fira <sarafira26@gmail.com>
2026-06-07 13:45:50 +0300

    Update script.js

--- free-code-camp/leap-year-calculator/script.js
+++ free-code-camp/leap-year-calculator/script.js
@@ -1,1 +1,13 @@
+let year = 2024;
 
+function isLeapYear(year) {
+  if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
+    return `${year} is a leap year.`;
+  } else {
+    return `${year} is not a leap year.`;
+  }
+}
+
+let result = isLeapYear(year);
+
+console.log(result);
