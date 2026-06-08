89f6ce4f31b8472cecf99ba73c1f2240fcd6ba59
Sara Fira <sarafira26@gmail.com>
2026-06-08 19:42:32 +0300

    Update script.js

--- codecademy/rock-paper-scissors/script.js
+++ codecademy/rock-paper-scissors/script.js
@@ -1,1 +1,64 @@
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+if ((userInput === 'rock') || (userInput === 'paper') || (userInput === 'scissors') || userInput === 'bomb') {
+  return userInput;
+} else {
+  return 'Incorrect input.';
+}
+};
 
+const getComputerChoice = () => {
+let number = Math.floor(Math.random() * 3);
+if (number === 0) {
+  return 'rock'
+} else if (number === 1) {
+ return 'paper'
+} else if (number === 2) {
+  return 'scissors'
+}
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+if (userChoice === 'bomb') {
+  return 'User won';
+}
+
+if (userChoice === computerChoice) {
+    return 'The game was a tie';
+}
+
+if (userChoice === 'rock') {
+  if (computerChoice === 'paper') {
+    return 'Computer won';
+  } else {
+    return 'User won';
+  }
+}
+
+if (userChoice === 'paper') {
+  if (computerChoice === 'scissors') {
+    return 'Computer won';
+  } else {
+    return 'User won';
+  }
+}
+
+if (userChoice === 'scissors') {
+  if (computerChoice === 'rock') {
+    return 'Computer won';
+  } else {
+    return 'User won';
+  }
+}
+};
+
+function playGame() {
+  let userChoice = getUserChoice('rock');
+  let computerChoice = getComputerChoice();
+
+console.log(`User chose: ${userChoice}`);
+  console.log(`Computer chose: ${computerChoice}`);
+  console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame();
