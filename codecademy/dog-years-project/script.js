c24bef3e357c6ea9ef68e745f776c7adf4999575
Sara Fira <sarafira26@gmail.com>
2026-06-08 14:24:49 +0300

    Update script.js

--- codecademy/dog-years-project/script.js
+++ codecademy/dog-years-project/script.js
@@ -1,1 +1,23 @@
-a
+// Variable myAge contains my current age
+const myAge = 30;
+
+// Variable earlyYears equals to 2
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+// Variable laterYears substracts 2 from myAge
+let laterYears = myAge - 2;
+
+// Variable laterYears is now recalculated
+laterYears *= 4;
+
+// Variable myAgeInDogYears shows my age converted to dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+// Using built-in method for lower case
+let myName = "Sara";
+myName = myName.toLowerCase();
+
+// Statement with my age in dog years and my name
+console.log(`My name is ${myName}. I am ${myAge} years old in human years, which is ${myAgeInDogYears} years old in dog years.`);
