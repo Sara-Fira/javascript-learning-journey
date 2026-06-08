1b9a1bb92e17c37405cdde948484728beb13728a
Sara Fira <sarafira26@gmail.com>
2026-06-08 14:09:30 +0300

    Update script.js

--- codecademy/kelvin-weather-project/script.js
+++ codecademy/kelvin-weather-project/script.js
@@ -1,1 +1,13 @@
-a
+// Variable kelvin shows today's temperature
+const kelvin = 0;
+
+// Variable celsius shows today's temperature in Celsius degrees
+const celsius = kelvin - 273;
+
+// Variable fahrenheit shows today's temperature in Fahrenheit degrees
+let fahrenheit = celsius * (9/5) + 32;
+
+// Here we use .floor() method to round down the Fahrenheit temperature
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
