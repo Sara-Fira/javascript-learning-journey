59f4f1e30b6fc588255da8a27f19fb183c36d8e2
Sara Fira <sarafira26@gmail.com>
2026-06-22 22:38:12 +0300

    Add access check for user role and activity status

---
+++ javascript-for-qa-automation/variables-conditionals-functions/access-check.js
@@ -0,0 +1,8 @@
+const userRole = 'admin';
+const isUserActive = true;
+
+if (userRole === 'admin' && isUserActive === true) {
+  console.log('Access granted');
+} else {
+  console.log('Access denied');
+}
