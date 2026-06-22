a93296d0185d53eb6faadadee58ca9a83fb5a79d
Sara Fira <sarafira26@gmail.com>
2026-06-22 22:35:12 +0300

    Implement login result check in login-result-check.js
    Add login result check for success and error cases.

---
+++ javascript-for-qa-automation/variables-conditionals-functions/login-result-check.js
@@ -0,0 +1,17 @@
+const username = 'standard_user';
+const expectedResult = 'success';
+const actualResult = 'success';
+const actualResult2 = 'error';
+
+if (actualResult === expectedResult) {
+  console.log('Login test passed');
+} else {
+  console.log('Login test failed');
+}
+
+if (actualResult2 === expectedResult) {
+  console.log('Login test passed');
+} else {
+  console.log('Login test failed');
+}
+
