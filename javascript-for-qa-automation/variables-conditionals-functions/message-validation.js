9943465d62178169c6cc9e9746757ec485b9f3b6
Sara Fira <sarafira26@gmail.com>
2026-06-22 22:36:38 +0300

    Add message validation for user greeting

---
+++ javascript-for-qa-automation/variables-conditionals-functions/message-validation.js
@@ -0,0 +1,8 @@
+const expectedMessage = 'Welcome, standard_user!';
+const actualMessage = 'Welcome, standard_user!';
+
+if (actualMessage === expectedMessage) {
+  console.log('Message validation passed');
+} else {
+  console.log('Message validation failed');
+}
