86d59d6920a1decf90959d9cb2ee2222deb363a6
Sara Fira <sarafira26@gmail.com>
2026-06-22 22:37:50 +0300

    Create status-code-validation.js

---
+++ javascript-for-qa-automation/variables-conditionals-functions/status-code-validation.js
@@ -0,0 +1,8 @@
+const expectedStatusCode = 200;
+const actualStatusCode = 404;
+
+if (actualStatusCode === expectedStatusCode) {
+  console.log('Status code test passed');
+} else {
+  console.log('Status code test failed');
+}
