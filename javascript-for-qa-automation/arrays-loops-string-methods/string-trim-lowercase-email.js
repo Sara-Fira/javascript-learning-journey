e168099b27dc14140f52d3f0376f4e317f980bcd
Sara Fira <sarafira26@gmail.com>
2026-06-23 21:43:01 +0300

    Create string-trim-lowercase-email.js

---
+++ javascript-for-qa-automation/arrays-loops-string-methods/string-trim-lowercase-email.js
@@ -0,0 +1,7 @@
+const enteredEmail = ' TEST@EXAMPLE.COM ';
+
+if (enteredEmail.trim().toLowerCase() === 'test@example.com') {
+  console.log('Email is valid');
+} else {
+  console.log('Email is invalid');
+}
