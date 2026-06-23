a0f27b8e26d111fdff9543a573bd4f14fdfc6e68
Sara Fira <sarafira26@gmail.com>
2026-06-23 21:42:32 +0300

    Add username validation with trimming

---
+++ javascript-for-qa-automation/arrays-loops-string-methods/string-trim-username.js
@@ -0,0 +1,7 @@
+const enteredUsername = ' standard_user ';
+
+if (enteredUsername.trim() === 'standard_user') {
+  console.log('Username is valid');
+} else {
+  console.log('Username is invalid');
+}
