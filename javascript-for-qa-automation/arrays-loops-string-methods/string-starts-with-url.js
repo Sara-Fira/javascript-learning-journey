f24635d046143a80c945025829c62901521f1f57
Sara Fira <sarafira26@gmail.com>
2026-06-23 21:41:45 +0300

    Add URL check for expected path using startsWith

---
+++ javascript-for-qa-automation/arrays-loops-string-methods/string-starts-with-url.js
@@ -0,0 +1,7 @@
+const currentUrl = '/inventory.html';
+
+if (currentUrl.startsWith('/inventory')) {
+  console.log('URL starts with expected path');
+} else {
+  console.log('URL does not start with expected path');
+}
