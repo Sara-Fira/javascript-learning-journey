62ee33332a65feeb245eb0be3f40a36d548fbf9a
Sara Fira <sarafira26@gmail.com>
2026-06-29 14:56:52 +0300

    Create map-usernames-arrow-function.js

---
+++ javascript-for-qa-automation/array-methods-for-qa/map-usernames-arrow-function.js
@@ -0,0 +1,9 @@
+const users = [
+  { username: 'standard_user', isActive: true },
+  { username: 'locked_out_user', isActive: false },
+  { username: 'problem_user', isActive: true }
+];
+
+const usernames = users.map(user => user.username);
+
+console.log(usernames);
