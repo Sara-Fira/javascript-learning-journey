bdc5cea0dd86cb712fc35815d3be9c46a06bcefe
Sara Fira <sarafira26@gmail.com>
2026-06-30 14:48:27 +0300

    Create foreach-test-names.js

---
+++ javascript-for-qa-automation/array-methods-mixed-practice/foreach-test-names.js
@@ -0,0 +1,10 @@
+const tests = [
+  { name: 'Login test', status: 'passed' },
+  { name: 'Checkout test', status: 'failed' },
+  { name: 'Cart test', status: 'passed' },
+  { name: 'Payment test', status: 'failed' }
+];
+
+tests.forEach(function(test) {
+  console.log(test.name);
+});
