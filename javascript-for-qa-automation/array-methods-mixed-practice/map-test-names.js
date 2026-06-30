31a05059e6ea8f5e60ab4fbed266a74aa275c0d7
Sara Fira <sarafira26@gmail.com>
2026-06-30 14:49:23 +0300

    Create map-test-names.js

---
+++ javascript-for-qa-automation/array-methods-mixed-practice/map-test-names.js
@@ -0,0 +1,12 @@
+const tests = [
+  { name: 'Login test', status: 'passed' },
+  { name: 'Checkout test', status: 'failed' },
+  { name: 'Cart test', status: 'passed' },
+  { name: 'Payment test', status: 'failed' }
+];
+
+const testNames = tests.map(function(test) {
+  return test.name;
+});
+
+console.log(testNames);
