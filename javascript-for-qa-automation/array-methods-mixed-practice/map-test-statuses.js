2077b715f4f5087e97149827e63d3d75f0b67563
Sara Fira <sarafira26@gmail.com>
2026-06-30 14:51:08 +0300

    Create map-test-statuses.js

---
+++ javascript-for-qa-automation/array-methods-mixed-practice/map-test-statuses.js
@@ -0,0 +1,12 @@
+const tests = [
+  { name: 'Login test', status: 'passed' },
+  { name: 'Checkout test', status: 'failed' },
+  { name: 'Cart test', status: 'passed' },
+  { name: 'Payment test', status: 'failed' }
+];
+
+const testStatuses = tests.map(function(test) {
+  return test.status;
+});
+
+console.log(testStatuses);
