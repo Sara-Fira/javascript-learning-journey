7ba9b8f0f33403bae6867e5e1a24a2656f180bc5
Sara Fira <sarafira26@gmail.com>
2026-07-03 14:48:17 +0300

    Create check-users-array-not-empty.js

---
+++ javascript-for-qa-automation/json-api-response-data/check-users-array-not-empty.js
@@ -0,0 +1,14 @@
+const response = {
+  status: 200,
+  body: {
+    users: [
+      { username: 'standard_user', isActive: true },
+      { username: 'locked_out_user', isActive: false },
+      { username: 'problem_user', isActive: true }
+    ]
+  }
+};
+
+const hasUsers = response.body.users.length > 0;
+
+console.log(hasUsers);
