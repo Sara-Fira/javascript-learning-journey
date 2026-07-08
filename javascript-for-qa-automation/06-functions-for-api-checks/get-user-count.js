ab9ad3357c6cac128510bdf194c87655c704a258
Sara Fira <sarafira26@gmail.com>
2026-07-08 14:46:39 +0300

    Add getUserCount function to count active users

---
+++ javascript-for-qa-automation/06-functions-for-api-checks/get-user-count.js
@@ -0,0 +1,16 @@
+const response = {
+  status: 200,
+  body: {
+    users: [
+      { username: 'standard_user', isActive: true },
+      { username: 'locked_out_user', isActive: false },
+      { username: 'problem_user', isActive: true }
+    ]
+  }
+};
+
+function getUserCount(response) {
+  return response.body.users.length;
+}
+
+console.log(getUserCount(response));
