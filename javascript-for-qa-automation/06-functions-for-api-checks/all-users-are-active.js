351956f972127a88e1692516ee6845420ff4ab62
Sara Fira <sarafira26@gmail.com>
2026-07-08 14:46:54 +0300

    Create all-users-are-active.js

---
+++ javascript-for-qa-automation/06-functions-for-api-checks/all-users-are-active.js
@@ -0,0 +1,18 @@
+const response = {
+  status: 200,
+  body: {
+    users: [
+      { username: 'standard_user', isActive: true },
+      { username: 'locked_out_user', isActive: false },
+      { username: 'problem_user', isActive: true }
+    ]
+  }
+};
+
+function allUsersAreActive(response) {
+  return response.body.users.every(function(user) {
+    return user.isActive === true;
+  });
+}
+
+console.log(allUsersAreActive(response));
