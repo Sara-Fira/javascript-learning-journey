56559bd11d5b0443aabc2aa1007785a880b110a2
Sara Fira <sarafira26@gmail.com>
2026-07-08 14:45:05 +0300

    Create get-active-usernames.js

---
+++ javascript-for-qa-automation/06-functions-for-api-checks/get-active-usernames.js
@@ -0,0 +1,22 @@
+const response = {
+  status: 200,
+  body: {
+    users: [
+      { username: 'standard_user', isActive: true },
+      { username: 'locked_out_user', isActive: false },
+      { username: 'problem_user', isActive: true }
+    ]
+  }
+};
+
+function getActiveUsernames(response) {
+  return response.body.users
+    .filter(function(user) {
+      return user.isActive === true;
+    })
+    .map(function(user) {
+      return user.username;
+    });
+}
+
+console.log(getActiveUsernames(response));
