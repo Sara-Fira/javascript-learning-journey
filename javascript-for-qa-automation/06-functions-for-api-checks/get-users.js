e4b452aab69eba547562d61d4e97850ba0a8e44a
Sara Fira <sarafira26@gmail.com>
2026-07-08 14:44:27 +0300

    Add getUsers function to retrieve user data

---
+++ javascript-for-qa-automation/06-functions-for-api-checks/get-users.js
@@ -0,0 +1,16 @@
+const response = {
+  status: 200,
+  body: {
+    users: [
+      { username: 'standard_user', isActive: true },
+      { username: 'locked_out_user', isActive: false },
+      { username: 'problem_user', isActive: true }
+    ]
+  }
+};
+
+function getUsers(response) {
+  return response.body.users;
+}
+
+console.log(getUsers(response));
